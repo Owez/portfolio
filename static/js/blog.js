6061204271315a0a34989fa1d9a1b9d278d9775b
Owen Griffiths <root@ogriffiths.com>
2022-11-18 18:50:18 +0000

    Redone a lot :)

--- static/js/blog.js
+++ static/js/blog.js
@@ -10,15 +10,21 @@ let jank = e.textContent.replace(/(\r\n|\n|\r)/gm, "").replace(/ +/gm, " ").repl
 // Remove jank element
 e.remove();
 
-// Put each article in buckets
+// Put each article in buckets, also take note of most recent post
 let buckets = {};
+let most_recent = { date_cmp: new Date(null) }
 for (let article of jank) {
     // Split into information
     let [name, date, link, categories] = article.split("=")
     categories = categories.trim().split(" ");
 
     // Assemble into article object
-    let assembled = { name: name, date: date, link: link };
+    let assembled = { name: name, date: date, date_cmp: new Date(date), link: link };
+
+    // Try to knock out most recent
+    if (assembled.date_cmp > most_recent.date_cmp) {
+        most_recent = assembled
+    }
 
     // Add to buckets
     for (let category of categories) {
@@ -29,6 +35,12 @@ for (let article of jank) {
         buckets[category].push(assembled);
     }
 }
+
+// Set most recent in latest card
+let latest_text = document.getElementById("latest-text")
+let latest_link = document.getElementById("latest-link")
+latest_text.innerHTML = most_recent.name;
+latest_link.href = most_recent.link
 
 // Assemble buckets into html
 for (category in buckets) {
@@ -47,8 +59,6 @@ for (category in buckets) {
 
     // Add articles
     for (let article of buckets[category]) {
-        console.log(article)
-
         // Make article element
         let ea = document.createElement("a");
         ea.href = article["link"];
